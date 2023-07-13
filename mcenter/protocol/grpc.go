00a42abf236e849c187e7b42770b7319dde778a3
solodba <945623397@qq.com>
2023-07-13 21:49:27 +0800

    添加GRPC启动和停止功能

---
+++ mcenter/protocol/grpc.go
@@ -0,0 +1,54 @@
+package protocol
+
+import (
+	"net"
+
+	"github.com/solodba/devcloud/tree/main/mcenter/apps"
+	"github.com/solodba/devcloud/tree/main/mcenter/common/logger"
+	"github.com/solodba/devcloud/tree/main/mcenter/conf"
+	"google.golang.org/grpc"
+)
+
+// GRPC服务结构体
+type GrpcService struct {
+	svr *grpc.Server
+	c   *conf.Config
+}
+
+// GRPC服务结构体初始化函数
+func NewGrpcService() *GrpcService {
+	srv := grpc.NewServer()
+	return &GrpcService{
+		svr: srv,
+		c:   conf.C(),
+	}
+}
+
+// GRPC服务启动方法
+func (s *GrpcService) Start() error {
+	// 初始化GRPC服务配置
+	if err := apps.InitGrpcApps(s.svr); err != nil {
+		logger.L().Info().Msgf("initial grpc object config error, err: %s", err.Error())
+		return err
+	}
+	listener, err := net.Listen("tcp", s.c.App.Grpc.Addr())
+	if err != nil {
+		logger.L().Info().Msgf("tcp listener error, err: %s", err.Error())
+		return err
+	}
+	err = s.svr.Serve(listener)
+	if err != nil {
+		if err == grpc.ErrServerStopped {
+			logger.L().Info().Msgf("grpc service is stopped")
+		}
+		logger.L().Info().Msgf("start grpc service error, err: %s", err.Error())
+		return err
+	}
+	return nil
+}
+
+// GRPC服务停止方法
+func (s *GrpcService) Stop() error {
+	s.svr.GracefulStop()
+	return nil
+}

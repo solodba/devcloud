8d7dabf27eb9aa8b9555a12035fbdd89e56d9805
code-horse-mi <945623397@qq.com>
2023-07-14 21:02:03 +0800

    添加pod业务功能实现类

---
+++ mpaas/apps/pod/impl/impl.go
@@ -0,0 +1,52 @@
+package impl
+
+import (
+	"github.com/solodba/devcloud/tree/main/mpaas/apps/pod"
+	"github.com/solodba/devcloud/tree/main/mpaas/conf"
+	"github.com/solodba/mcube/apps"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
+	"k8s.io/client-go/kubernetes"
+)
+
+var (
+	svc = &impl{}
+)
+
+// 业务实现类
+type impl struct {
+	pod.UnimplementedRPCServer
+	col       *mongo.Collection
+	clientSet *kubernetes.Clientset
+}
+
+// 实现Ioc中心Name方法
+func (i *impl) Name() string {
+	return pod.AppName
+}
+
+// 实现Ioc中心Conf方法
+func (i *impl) Conf() error {
+	db, err := conf.C().MongoDB.GetDB()
+	if err != nil {
+		return err
+	}
+	i.col = db.Collection("pods")
+	clientSet, err := conf.C().K8s.GetK8sConn()
+	if err != nil {
+		return err
+	}
+	i.clientSet = clientSet
+	return nil
+}
+
+// 实现Ioc中心RegistryHandler方法
+func (i *impl) RegistryHandler(s *grpc.Server) {
+	pod.RegisterRPCServer(s, svc)
+}
+
+// 注册到Ioc中心
+func init() {
+	apps.RegistryInternalApp(svc)
+	apps.RegistryGrpcApp(svc)
+}
--- mpaas/apps/pod/interface.go
+++ mpaas/apps/pod/interface.go
@@ -1,9 +1,5 @@
 package pod
 
-import (
-	context "context"
-)
-
 // 模块名称
 const (
 	AppName = "pod"
@@ -11,15 +7,5 @@ const (
 
 // 定义业务接口
 type Service interface {
-	// 创建Pod
-	CreatePod(context.Context, *CreatePodRequest) (*Pod, error)
-	// 删除Pod
-	DeletePod(context.Context, *DeletePodRequest) (*Pod, error)
-	// 修改Pod
-	UpdatePod(context.Context, *UpdatePodRequest) (*Pod, error)
-	// 查询Pod
-	QueryPod(context.Context, *QueryPodRequest) (*PodSet, error)
-	// 查询Pod详情
-	DescribePod(context.Context, *DescribePodRequest) (*Pod, error)
 	RPCServer
 }

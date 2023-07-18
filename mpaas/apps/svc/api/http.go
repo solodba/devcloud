16ffccce4bc44641ce86552baa710ad46c3d901c
solodba <945623397@qq.com>
2023-07-18 17:49:06 +0800

    添加Service的restful APII

---
+++ mpaas/apps/svc/api/http.go
@@ -0,0 +1,75 @@
+package api
+
+import (
+	restfulspec "github.com/emicklei/go-restful-openapi"
+	"github.com/emicklei/go-restful/v3"
+	"github.com/solodba/devcloud/mpaas/apps/svc"
+	"github.com/solodba/mcube/apps"
+)
+
+// handler结构体
+type handler struct {
+	svc svc.SvcService
+}
+
+// 实现注册restful实例Name方法
+func (h *handler) Name() string {
+	return svc.AppName
+}
+
+// 实现注册restful实例Conf方法
+func (h *handler) Conf() error {
+	// 从IOC获取Service实例
+	h.svc = apps.GetInternalApp(svc.AppName).(svc.SvcService)
+	return nil
+}
+
+// 实现注册restful实例version方法
+func (h *handler) Version() string {
+	return "v1"
+}
+
+// 实现注册restful实例Registry方法
+func (h *handler) RegistryHandler(ws *restful.WebService) {
+	tags := []string{"Service管理"}
+	// webservice定义路由信息
+	// 创建Service
+	ws.Route(ws.POST("/").To(h.CreateService).
+		Doc("创建Service").
+		Metadata(restfulspec.KeyOpenAPITags, tags).
+		Reads(svc.CreateServiceRequest{}).
+		Writes(svc.Service{}).
+		Returns(200, "OK", svc.Service{}))
+
+	// 删除Service
+	ws.Route(ws.DELETE("/{namespace}/{name}").To(h.DeleteService).
+		Doc("删除Service").
+		Metadata(restfulspec.KeyOpenAPITags, tags).
+		Reads(svc.DeleteServiceRequest{}).
+		Writes(svc.Service{}).
+		Returns(200, "OK", svc.Service{}))
+
+	// 更新Service
+	ws.Route(ws.PUT("/").To(h.UpdateService).
+		Doc("更新Service").
+		Metadata(restfulspec.KeyOpenAPITags, tags).
+		Reads(svc.UpdateServiceRequest{}).
+		Writes(svc.Service{}).
+		Returns(200, "OK", svc.Service{}))
+
+	// 查询Service
+	ws.Route(ws.GET("/{namespace}").To(h.QueryOrDescribeService).
+		Doc("查询Service").
+		Metadata(restfulspec.KeyOpenAPITags, tags).
+		Reads(svc.QueryServiceRequest{}).
+		Writes(svc.ServiceSet{}).
+		Returns(200, "OK", svc.ServiceSet{}).
+		Reads(svc.DescribeServiceRequest{}).
+		Writes(svc.Service{}).
+		Returns(200, "OK", svc.Service{}))
+}
+
+// 初始化函数注册restful实例
+func init() {
+	apps.RegistryRestfulApp(&handler{})
+}
---
+++ mpaas/apps/svc/api/svc.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/emicklei/go-restful/v3"
+
+// 创建Service
+func (h *handler) CreateService(r *restful.Request, w *restful.Response) {
+
+}
+
+// 创建Service
+func (h *handler) DeleteService(r *restful.Request, w *restful.Response) {
+
+}
+
+// 创建Service
+func (h *handler) UpdateService(r *restful.Request, w *restful.Response) {
+
+}
+
+// 创建Service
+func (h *handler) QueryOrDescribeService(r *restful.Request, w *restful.Response) {
+
+}

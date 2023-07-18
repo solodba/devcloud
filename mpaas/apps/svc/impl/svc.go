dda8a98d263d0b69ba8b9c85978c4564c7bca6b0
solodba <945623397@qq.com>
2023-07-18 11:55:00 +0800

    添加Service业务功能框架

---
+++ mpaas/apps/svc/impl/svc.go
@@ -0,0 +1,32 @@
+package impl
+
+import (
+	"context"
+
+	"github.com/solodba/devcloud/mpaas/apps/svc"
+)
+
+// 创建Service
+func (i *impl) CreateService(ctx context.Context, in *svc.CreateServiceRequest) (*svc.Service, error) {
+	return nil, nil
+}
+
+// 删除Service
+func (i *impl) DeleteService(ctx context.Context, in *svc.DeleteServiceRequest) (*svc.Service, error) {
+	return nil, nil
+}
+
+// 修改Service
+func (i *impl) UpdateService(ctx context.Context, in *svc.UpdateServiceRequest) (*svc.Service, error) {
+	return nil, nil
+}
+
+// 查询Service
+func (i *impl) QueryService(ctx context.Context, in *svc.QueryServiceRequest) (*svc.ServiceSet, error) {
+	return nil, nil
+}
+
+// 查询Service详情
+func (i *impl) DescribeService(ctx context.Context, in *svc.DescribeServiceRequest) (*svc.Service, error) {
+	return nil, nil
+}

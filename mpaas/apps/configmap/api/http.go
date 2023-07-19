1b7c22c319686ebdc358a23a826acbf65cf45115
solodba <945623397@qq.com>
2023-07-19 15:28:59 +0800

    添加ConfigMap Restful API接口功能框架

---
+++ mpaas/apps/configmap/api/http.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/emicklei/go-restful/v3"
+
+// 创建ConfigMap
+func (h *handler) CreateConfigMap(r *restful.Request, w *restful.Response) {
+
+}
+
+// 删除ConfigMap
+func (h *handler) DeleteConfigMap(r *restful.Request, w *restful.Response) {
+
+}
+
+// 修改ConfigMap
+func (h *handler) UpdateConfigMap(r *restful.Request, w *restful.Response) {
+
+}
+
+// 查询ConfigMap
+func (h *handler) QueryOrDescribeConfigMap(r *restful.Request, w *restful.Response) {
+
+}

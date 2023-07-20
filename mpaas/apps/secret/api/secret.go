96e8d6e65c29e8d7be30786dc91e7405f03b16d8
solodba <945623397@qq.com>
2023-07-20 22:14:43 +0800

    添加Secret Restful API接口功能

---
+++ mpaas/apps/secret/api/secret.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/emicklei/go-restful/v3"
+
+// 创建Secret
+func (h *handler) CreateSecret(r *restful.Request, w *restful.Response) {
+
+}
+
+// 删除Secret
+func (h *handler) DeleteSecret(r *restful.Request, w *restful.Response) {
+
+}
+
+// 更新Secret
+func (h *handler) UpdateSecret(r *restful.Request, w *restful.Response) {
+
+}
+
+// 查询Secret
+func (h *handler) QueryOrDescribeSecret(r *restful.Request, w *restful.Response) {
+
+}

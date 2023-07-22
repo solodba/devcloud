c5e76cc4107fa54e483014ccfc3a1b259cd72348
code-horse-mi <945623397@qq.com>
2023-07-22 23:21:22 +0800

    添加创建PersistentVolume Restful API接口功能

--- mpaas/apps/all/internal.go
+++ mpaas/apps/all/internal.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/solodba/devcloud/mpaas/apps/namespace/impl"
 	_ "github.com/solodba/devcloud/mpaas/apps/node/impl"
 	_ "github.com/solodba/devcloud/mpaas/apps/pod/impl"
+	_ "github.com/solodba/devcloud/mpaas/apps/pv/impl"
 	_ "github.com/solodba/devcloud/mpaas/apps/secret/impl"
 	_ "github.com/solodba/devcloud/mpaas/apps/svc/impl"
 )
--- mpaas/apps/all/restful.go
+++ mpaas/apps/all/restful.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/solodba/devcloud/mpaas/apps/namespace/api"
 	_ "github.com/solodba/devcloud/mpaas/apps/node/api"
 	_ "github.com/solodba/devcloud/mpaas/apps/pod/api"
+	_ "github.com/solodba/devcloud/mpaas/apps/pv/api"
 	_ "github.com/solodba/devcloud/mpaas/apps/secret/api"
 	_ "github.com/solodba/devcloud/mpaas/apps/svc/api"
 	_ "github.com/solodba/devcloud/mpaas/apps/test/api"

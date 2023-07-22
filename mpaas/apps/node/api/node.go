13f0ad1cf2d549cf325bf2ecdfe1c48cb003598a
code-horse-mi <945623397@qq.com>
2023-07-22 22:51:13 +0800

    添加Node Restfule API功能

--- mpaas/apps/node/api/node.go
+++ mpaas/apps/node/api/node.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/emicklei/go-restful/v3"
 	"github.com/solodba/devcloud/mpaas/apps/node"
 	"github.com/solodba/mcube/response"
@@ -30,7 +32,18 @@ func (h *handler) QueryOrDescribeNode(r *restful.Request, w *restful.Response) {
 
 // 更新节点标签
 func (h *handler) UpdateNodeLabel(r *restful.Request, w *restful.Response) {
-
+	req := node.NewUpdatedLabelRequest()
+	err := r.ReadEntity(req)
+	if err != nil {
+		w.WriteEntity(response.NewFail(400, err.Error()))
+		return
+	}
+	_, err = h.svc.UpdateNodeLabel(r.Request.Context(), req)
+	if err != nil {
+		w.WriteEntity(response.NewFail(500, err.Error()))
+		return
+	}
+	w.WriteEntity(response.NewSuccess(200, fmt.Sprintf("[%s]节点标签更新成功", req.Name)))
 }
 
 // 更新节点污点

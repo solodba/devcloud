b37a39fa5732a214029de94c79d63607d4b276f4
code-horse-mi <945623397@qq.com>
2023-07-23 14:33:23 +0800

    添加StorageClass Restful API实现类和方法

---
+++ mpaas/apps/sc/api/sc.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/emicklei/go-restful/v3"
+)
+
+// 创建StorageClass
+func (h *handler) CreateSC(r *restful.Request, w *restful.Response) {
+
+}
+
+// 删除StorageClass
+func (h *handler) DeleteSC(r *restful.Request, w *restful.Response) {
+
+}
+
+// 查询StorageClass集合
+func (h *handler) QuerySC(r *restful.Request, w *restful.Response) {
+
+}

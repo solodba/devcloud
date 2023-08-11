12362e1a477de856c8d7ed5f09eab3846757f548
code-horse-mi <945623397@qq.com>
2023-08-11 16:12:46 +0800

    添加Service业务接口实现类和方法

---
+++ mcenter/apps/service/impl/service.go
@@ -0,0 +1,22 @@
+package impl
+
+import (
+	"context"
+
+	"github.com/solodba/devcloud/mcenter/apps/service"
+)
+
+// 创建服务
+func (i *impl) CreateService(ctx context.Context, in *service.CreateServiceRequest) (*service.Service, error) {
+	return nil, nil
+}
+
+// 查询服务
+func (i *impl) QueryService(ctx context.Context, in *service.QueryServiceRequest) (*service.ServiceSet, error) {
+	return nil, nil
+}
+
+// 查询服务详情
+func (i *impl) DescribeService(ctx context.Context, in *service.DescribeServiceRequest) (*service.Service, error) {
+	return nil, nil
+}

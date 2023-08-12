ce58f30d81b1d03eb3fbba93651e5c5236f1ccf0
code-horse-mi <945623397@qq.com>
2023-08-12 12:51:41 +0800

    添加http认证中间件, 主要用于用户的鉴权

--- mcenter/protocol/auth/http.go
+++ mcenter/protocol/auth/http.go
@@ -1,1 +1,27 @@
 package auth
+
+import (
+	"github.com/emicklei/go-restful/v3"
+	"github.com/solodba/devcloud/mcenter/apps/token"
+	"github.com/solodba/mcube/apps"
+)
+
+// httpAuther认证结构体
+type httpAuther struct {
+	svc token.Service
+}
+
+// httpAuther初始化函数
+func NewHttpAuther() *httpAuther {
+	return &httpAuther{
+		svc: apps.GetInternalApp(token.AppName).(token.Service),
+	}
+}
+
+// type FilterFunction func(*Request, *Response, *FilterChain)
+// 实现上述函数就是http中间件
+func (a *httpAuther) AuthFunc(req *restful.Request, resp *restful.Response, next *restful.FilterChain) {
+	// 获取http认证token信息
+	// tokenHeader := req.HeaderParameter(token.AUTH_TOKEN_KEY)
+	// tokenHeaderList := strings.Split(tokenHeader, " ")
+}

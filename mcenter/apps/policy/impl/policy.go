e5e67e69b14d20afd89ce60317c25978ab13a09c
solodba <945623397@qq.com>
2023-08-14 22:05:41 +0800

    添加Policy业务接口实现类和方法

---
+++ mcenter/apps/policy/impl/policy.go
@@ -0,0 +1,17 @@
+package impl
+
+import (
+	"context"
+
+	"github.com/solodba/devcloud/mcenter/apps/policy"
+)
+
+// 创建Policy
+func (i *impl) CreatePolicy(ctx context.Context, in *policy.CreatePolicyRequest) (*policy.Policy, error) {
+	return nil, nil
+}
+
+// 查询Policy
+func (i *impl) QueryPolicy(ctx context.Context, in *policy.QueryPolicyRequest) (*policy.PolicySet, error) {
+	return nil, nil
+}
